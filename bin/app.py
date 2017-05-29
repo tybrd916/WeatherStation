c76c104477ba0dd4ba4f52c7de188043d88436b3
tybrd916 <tyler.m.carr@gmail.com>
2017-05-29 18:36:30 -0400

    Experimenting with matplotlib to draw a chart

--- bin/app.py
+++ bin/app.py
@@ -13,13 +13,46 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageOps
+import numpy
 import sys
+import matplotlib
+matplotlib.use("pdf")
+import matplotlib.pyplot as plt
 
 if len(sys.argv) < 3:
   print "You MUST supply a port and weatherunderground API key"
   exit(2)
 
 apikey=sys.argv[2]
+
+def fig2data ( fig ):
+    """
+    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
+    @param fig a matplotlib figure
+    @return a numpy 3D array of RGBA values
+    """
+    # draw the renderer
+    fig.canvas.draw ( )
+ 
+    # Get the RGBA buffer from the figure
+    w,h = fig.canvas.get_width_height()
+    buf = numpy.fromstring ( fig.canvas.tostring_argb(), dtype=numpy.uint8 )
+    buf.shape = ( w, h,4 )
+ 
+    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+    buf = numpy.roll ( buf, 3, axis = 2 )
+    return buf
+
+def fig2img ( fig ):
+    """
+    @brief Convert a Matplotlib figure to a PIL Image in RGBA format and return it
+    @param fig a matplotlib figure
+    @return a Python Imaging Library ( PIL ) image
+    """
+    # put the figure pixmap into a numpy array
+    buf = fig2data ( fig )
+    w, h, d = buf.shape
+    return Image.fromstring( "RGBA", ( w ,h ), buf.tostring( ) )
 
 #requests_cache.install_cache('weatherunderground_cache', backend='sqlite', expire_after=1800)
 
@@ -68,27 +101,27 @@ class images:
 
         downloadfreshdata=1
         now = time.time()
-        #with open('/home/pi/WeatherPython/hourly10day.json', 'w') as hourlyforecast10day_outfile:
-        if os.path.isfile('/home/pi/WeatherPython/hourly10day.json'):
-          if os.stat('/home/pi/WeatherPython/hourly10day.json').st_mtime > now - 3600:
+        #with open('/home/pi/WeatherStation/hourly10day.json', 'w') as hourlyforecast10day_outfile:
+        if os.path.isfile('/home/pi/WeatherStation/hourly10day.json'):
+          if os.stat('/home/pi/WeatherStation/hourly10day.json').st_mtime > now - 3600:
             #print "file is new-ish, don't refresh!"
             downloadfreshdata=0
 
         print "tyler sees APIKEY = "+apikey
         if downloadfreshdata > 0 :
         	hourlyforecast10day = requests.get("http://api.wunderground.com/api/"+apikey+"/geolookup/hourly10day/q/VT/Williston.json").json()
-                with open('/home/pi/WeatherPython/hourly10day.json', 'w') as hourlyforecast10day_outfile:
+                with open('/home/pi/WeatherStation/hourly10day.json', 'w') as hourlyforecast10day_outfile:
                   json.dump(hourlyforecast10day, hourlyforecast10day_outfile)
 
-        #with open('/Users/tcarr/WeatherPython/conditions.json') as json_conditions_data:
+        #with open('/Users/tcarr/WeatherStation/conditions.json') as json_conditions_data:
         #    weatherconditions = json.load(json_conditions_data)
         	weatherconditions = requests.get("http://api.wunderground.com/api/"+apikey+"/geolookup/conditions/q/VT/Williston.json").json()
-                with open('/home/pi/WeatherPython/conditions.json', 'w') as conditions_outfile:
+                with open('/home/pi/WeatherStation/conditions.json', 'w') as conditions_outfile:
                   json.dump(weatherconditions, conditions_outfile)
         else:
-             with open('/home/pi/WeatherPython/hourly10day.json') as json_hourlyforecast10day_data:
+             with open('/home/pi/WeatherStation/hourly10day.json') as json_hourlyforecast10day_data:
                hourlyforecast10day = json.load(json_hourlyforecast10day_data)
-             with open('/home/pi/WeatherPython/conditions.json') as json_conditions_data:
+             with open('/home/pi/WeatherStation/conditions.json') as json_conditions_data:
                weatherconditions = json.load(json_conditions_data)
 
         forecastlabel="Today"
@@ -107,8 +140,18 @@ class images:
         else:
           noonhour=12
         noontemp=currtemp
+        tylerfile=open("/home/pi/WeatherStation/tyler.txt","w")
+        timelist=[]
+        epochlist=[]
+        preciplist=[]
         for i, entry in enumerate(hourlyforecast10day["hourly_forecast"]):
           hdate = entry['FCTTIME']['mon_padded']+"/"+entry['FCTTIME']['mday_padded']+"/"+entry['FCTTIME']['year']
+          if i <= 72:
+            timelist.append(datetime.datetime.fromtimestamp(float(entry['FCTTIME']['epoch'])))
+            epochlist.append(float(entry['FCTTIME']['epoch']))
+            preciplist.append(float(entry['pop']))
+            #tylerfile.write("Tyler sees:\n"+datetime.datetime.fromtimestamp(float(entry['FCTTIME']['epoch'])).strftime('%c'))
+            #tylerfile.write("Tyler sees:\n"+str(entry))
           if hdate == targetdatestr:
             hourlytemp = entry['temp']['english']
             if entry['FCTTIME']['hour_padded'] == noonhour:
@@ -118,6 +161,25 @@ class images:
               targetlow = hourlytemp
             if float(hourlytemp) > float(targethigh):
               targethigh = hourlytemp
+
+        #time_format = '%Y-%m-%d %H:%M'
+        #timelist2=[datetime.datetime.strftime(i, time_format) for i in timelist]
+        tylerfile.write(str(timelist)+"\n")
+        tylerfile.write(str(preciplist)+"\n")
+        #print "Before Plot attempt"
+        plt.plot(timelist, preciplist)
+        #print "Auto Plot attempt"
+        fig = plt.gcf()
+        fig.set_size_inches(18.5, 10.5)
+        fig.autofmt_xdate()
+        #fig.draw()
+        #buffer = StringIO.StringIO()
+        #plotImage = fig2img(fig)
+        #pil_image.save(buffer, 'PNG')
+        plt.xlabel('Precipitation Chance')
+        plt.ylabel('Date/Time')
+        plt.savefig('precipitationForecast.png')
+
         kid="WeatherGirl"
         if str(web.ctx.path) == "/alex.png":
           kid="WeatherBoy"
@@ -163,6 +225,7 @@ class images:
         #print im2.mode+" "+im1.mode
         im2.paste(im1,(250,100), mask=im1)
         #im2.paste(im1,(300,100))
+        #im2.paste(plotImage,5,5) #, mask=plotImage) #plot debug
 
         #Resolve battery level if passed
         param_data = web.input(batterylevel=None)
@@ -173,7 +236,7 @@ class images:
         # Drawing the text on the picture
         #draw = ImageDraw.Draw(im1)
         draw = ImageDraw.Draw(im2)
-        draw.text((20, 20),"Current Temperature",fontcolor,font=labelfont)
+        draw.text((20, 20),"Recent Temperature",fontcolor,font=labelfont)
         draw.text((20, 50),currtemp+degreesymbol+"F",fontcolor,font=temperaturefont)
         draw.text((20, 200),forecastlabel+" High",fontcolor,font=labelfont)
         draw.text((20, 230),str(targethigh)+degreesymbol+"F",fontcolor,font=temperaturefont)
