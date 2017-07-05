ab7ac71986dc8baaff72676a21fbc1e790e2bf40
tybrd916 <tyler.m.carr@gmail.com>
2017-07-05 14:13:56 -0400

    Fixing plt import in spite of Anaconda recommendation, which broke code.

--- bin/app.py
+++ bin/app.py
@@ -16,6 +16,7 @@ import numpy
 import sys
 import matplotlib
 matplotlib.use("agg")
+import matplotlib.pyplot as plt
 
 if len(sys.argv) < 3:
     print "You MUST supply a port and weatherunderground API key"
@@ -35,12 +36,13 @@ def fig2data(fig):
     fig.canvas.draw()
 
     # Get the RGBA buffer from the figure
-    w,h = fig.canvas.get_width_height()
-    buf = numpy.fromstring ( fig.canvas.tostring_argb(), dtype=numpy.uint8 )
-    buf.shape = ( w, h,4 )
- 
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = numpy.roll ( buf, 3, axis = 2 )
+    w, h = fig.canvas.get_width_height()
+    buf = numpy.fromstring(fig.canvas.tostring_argb(), dtype=numpy.uint8)
+    buf.shape = (w, h, 4)
+
+    # canvas.tostring_argb give pixmap in ARGB mode.
+    # Roll the ALPHA channel to have it in RGBA mode
+    buf = numpy.roll(buf, 3, axis=2)
     return buf
 
 def fig2img ( fig ):
@@ -170,8 +172,8 @@ class images:
         #tylerfile.write(str(timelist)+"\n")
         #tylerfile.write(str(preciplist)+"\n")
         #print "Before Plot attempt"
-        plt = None
-        plt = matplotlib.pyplot
+        ##plt = None
+        ##plt = matplotlib.pyplot
         #plt.plot(timelist, preciplist)
         #print "Auto Plot attempt"
         #fig = plt.gcf()
